e2c500b688c98f9e9d99650c57d287fba340112b
Michael Czuba <czuba.mich@gmail.com>
2020-10-10 19:26:40 +0200

    Initial Commit

---
+++ app/src/main/java/com/mczuba/covidtracker/ui/region/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.mczuba.covidtracker.ui.region
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
